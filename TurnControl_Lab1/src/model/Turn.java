a4109660744560a98232af3049b70e5e2e7fadba
Diego-ds <54712482+Diego-ds@users.noreply.github.com>
2020-02-08 15:00:56 -0500

    Turn class created

---
+++ TurnControl_Lab1/src/model/Turn.java
@@ -0,0 +1,27 @@
+package model;
+
+public class Turn {
+	private String turn;
+	private String status;
+	public final static String ATENDIDO ="Attended";
+	public final static String NO_ESTABA ="The user wasn't present";
+	public final static String NO_ATENDIDO ="Not attended yet";
+	public Turn(String turn,String status) {
+		this.turn=turn;
+		this.status=status;
+	}
+	public String getTurn() {
+		return turn;
+	}
+	public void setTurn(String turn) {
+		this.turn = turn;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	
+}

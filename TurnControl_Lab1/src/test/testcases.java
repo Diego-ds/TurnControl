a887e096739a5b029240c7fc11799fba92e5105a
usuario <usuario@usuario-PC>
2020-02-15 21:51:42 -0500

    test

--- TurnControl_Lab1/src/test/testcases.java
+++ TurnControl_Lab1/src/test/testcases.java
@@ -1,13 +1,25 @@
 package test;
-
+import model.*;
 import static org.junit.jupiter.api.Assertions.*;
-
+import CustomExceptions.*;
 import org.junit.jupiter.api.Test;
 
-class testcases {
 
+class testcases {
+	private User user;
+	private Turn turn;
+	private TurnManagement control;
+	
+	public void setUp1() {
+		
+		user = new User(User.CC,"384122","Diego","Garcia","31832324","cra 43 ");
+		user.setTurn(control.getActualTurn(), Turn.NO_ATENDIDO);
+		
+	}
+	
+	
 	@Test
-	void test() {
+	public void addUserTest() {
 		fail("Not yet implemented");
 	}
 
